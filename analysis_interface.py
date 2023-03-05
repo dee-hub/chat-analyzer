f7bcd6fbc5a49bb913796f3c6bb5d5be48a54b64
Salako Oluwadolapo <willdynamics@gmail.com>
2023-03-05 19:14:11 +0100

    Update analysis_interface.py

--- analysis_interface.py
+++ analysis_interface.py
@@ -198,7 +198,8 @@ def line_by_hour(data):
     return st.plotly_chart(fig, use_container_width=True)
 
 def collocation_extraction(data):
-    corpus = ' '.join(data['Message'])
+    df['Message'] = data['Message'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+    corpus = ' '.join(df['Message'])
 # Tokenize the corpus
     tokens = nltk.word_tokenize(corpus)
 # Remove stopwords
@@ -285,7 +286,8 @@ def word_frequency(data, selected_author):
     return st.plotly_chart(fig, use_container_width=True)
 
 def collocation_extraction_by_author(data, selected_author):
-    corpus = ' '.join(data.loc[data['Author'] == selected_author]['Message'])
+    df['Message'] = data['Message'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+    corpus = ' '.join(df.loc[df['Author'] == selected_author]['Message'])
 # Tokenize the corpus
     tokens = nltk.word_tokenize(corpus)
 # Remove stopwords
