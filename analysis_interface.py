bee82cecf8baee70368b8e60fefd05663e21ca4c
Salako Oluwadolapo <willdynamics@gmail.com>
2023-03-05 20:06:16 +0100

    Update analysis_interface.py

--- analysis_interface.py
+++ analysis_interface.py
@@ -465,7 +465,7 @@ if uploaded_file is not None:
             st.header('Sentiment Analysis')
             st.write(f"<p style='font-size:15px'>Sentiment analysis for Whatsapp Chats can reveal the emotional undertones of conversations and provide valuable insights into the feelings and attitudes of participants, enabling a better understanding of the dynamics of interpersonal relationships.</p>", unsafe_allow_html=True)
             sentiment_analysis_group(data, add_selectbox)
-            st.write(f"<p style='font-size:18px'>This analysis is based on {data1['Message'].count()} messages .</p>", unsafe_allow_html=True)
+            st.write(f"<p style='font-size:18px'>This analysis is based on {data['Message'].count()} messages .</p>", unsafe_allow_html=True)
                       
             
     else:
