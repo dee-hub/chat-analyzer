5e2e31adcb5bb2bfeb9b5444dbe4370052edc304
Salako Oluwadolapo <willdynamics@gmail.com>
2023-03-08 15:25:14 +0100

    Update analysis_interface.py

--- analysis_interface.py
+++ analysis_interface.py
@@ -213,7 +213,10 @@ def line_by_hour(data):
         try:
             data['DateTime'] = pd.to_datetime(data['DateTime'], format='%d/%m/%Y, %H:%M:%S')
         except ValueError:
-            data['DateTime'] = pd.to_datetime(data['DateTime'], format='%m/%d/%y, %I:%M %p')
+            try:
+                data['DateTime'] = pd.to_datetime(data['DateTime'], format='%m/%d/%y, %I:%M %p')
+            except ValueError:
+                date_time = pd.to_datetime(row['DateTime'], format='%d/%m/%Y, %I:%M:%S %p')
 # Group the messages by hour
     hourly_counts = data.groupby(data['DateTime'].dt.hour)['Message'].count().reset_index(name='Message Count')
     hourly_counts['DateTime'] = hourly_counts['DateTime'].map({0: '12AM', 1: '1AM', 2: '2AM', 3: '3AM', 4: '4AM', 5: '5AM',
